2431f1eb2071f8281ed57310e13d9a83c8bd437d
otyaaa <otyaaa03.gmail.com>
2026-04-22 06:01:29 +0900

    a

--- diary_data.js
+++ diary_data.js
@@ -1,7 +1,7 @@
 
 const allDiaries = [
-    { date: "20260422", title: "third", url: "diary/20260422.html" },
-    { date: "20260421", title: "third", url: "diary/20260421.html" },
+    { date: "20260422", title: "生きることと考えること", url: "diary/20260422.html" },
+    { date: "20260421", title: "恋愛", url: "diary/20260421.html" },
     { date: "20260420", title: "second", url: "diary/20260420.html" },
     { date: "20260419", title: "first", url: "diary/20260419.html" },
 ];

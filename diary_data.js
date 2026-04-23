0f2db9d21fbd536ef5dcf252bb61c959668e7d10
otyaaa03 <otyaaa03@gmail.com>
2026-04-24 01:50:30 +0900

    first commit

--- diary_data.js
+++ diary_data.js
@@ -1,5 +1,6 @@
 
 const allDiaries = [
+    { date: "20260423", title: "日常回 1", url: "diary/20260423.html" },
     { date: "20260422", title: "生きることと考えること", url: "diary/20260422.html" },
     { date: "20260421", title: "恋愛", url: "diary/20260421.html" },
     { date: "20260420", title: "second", url: "diary/20260420.html" },
